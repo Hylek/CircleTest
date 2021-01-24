a9eeed00d68c395d336b6fc24ca9fc512d2c9691
Daniel Cumbor <dcdolphy@gmail.com>
2021-01-24 13:48:40 +0000

    Created Volume script, created StateID enum.

--- Scripts/States/DefaultState.cs
+++ Scripts/States/DefaultState.cs
@@ -1,5 +1,13 @@
 using Godot;
 
+public enum StateID
+{
+    Default = 0,
+    Volume1 = 1,
+    Volume2 = 2,
+    Volume3 = 3
+}
+
 // The player's default white circle state when not in any volume.
 public class DefaultState : State
 {
---
+++ Scripts/Volume.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+// Handles logic for all Volumes.
+// Primarily sending custom signals on collision detection
+public class Volume : Node2D
+{
+    [Export]
+    public StateID id = StateID.Default;
+
+    [Signal]
+    public delegate void PlayerEnter(StateID id);
+
+    [Signal]
+    public delegate void PlayerExit();
+
+    public void onVolumeEnter(CollisionObject2D body)
+    {
+        EmitSignal(nameof(PlayerEnter), id);
+    }
+
+    public void OnVolumeExit(CollisionObject2D body)
+    {
+        EmitSignal(nameof(PlayerExit));
+    }
+}
